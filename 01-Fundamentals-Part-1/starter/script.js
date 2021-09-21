57a7c84cf637f29e5e2a818d2dcac03fbf58c52f
mark-escosura <escosuramarkse@gmail.com>
2021-09-20 17:54:29 -0700

    Finished Equality Operators == vs. ===

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -220,7 +220,7 @@ const description2 = `The ${country} is in ${continent} and it's ${usaPopulation
 
 // console.log(description2);
 
-// Taking Decisions: IF / ELSE Statements
+// ####################### Taking Decisions: IF / ELSE Statements #######################
 
 const yourName = `Mark`
 const age = 24;
@@ -243,7 +243,7 @@ if (birthYear <= 2000) { // returns either true or false because we are using a 
 
 // console.log(century);
 
-// Practice Assignment:
+// ####################### Practice Assignment: #######################
 
 /*
 1. If your country's population is greater that 33 million, log a string like this to the
@@ -252,18 +252,18 @@ console: 'Portugal's population is above average'. Otherwise, log a string like
 minus the country's population)
 */
 
-if (usaPopulation > averagePopulation) {
-    console.log(`${country}'s population is above average`);
-} else {
-    console.log(`${country}'s population is 22 million below average`);
-}
+// if (usaPopulation > 33) {
+//     console.log(`${country}'s population is above average`);
+// } else {
+//     console.log(`${country}'s population is ${33 - usaPopulation} million below average`);
+// }
 
 /*
 2. After checking the result, change the population temporarily to 13 and then to
 130. See the different results, and set the population back to original
 */
 
-// Type Conversion and Coercion
+// ####################### Type Conversion and Coercion #######################
 
 // Type Conversion
 
@@ -276,7 +276,7 @@ const inputYear = '1991';
 
 // console.log(String(23), 23);
 
-// Practice Assignment: 
+// ####################### Practice Assignment: #######################
 
 /*
 Predict the result of these 5 operations without executing them:
@@ -299,30 +299,113 @@ Predict the result of these 5 operations without executing them:
 // console.log('123' < 57);
 // console.log(5 + 6 + '4' + 9 - 4 - 2);
 
-// Type Coercion
+// Type Coercion 
 
-console.log('I am '+ 23 + ' years old.'); // all the numbers are converted to strings using the + operator
+// console.log('I am '+ 23 + ' years old.'); // all the numbers are converted to strings using the + operator
 
-// Truthy and Falsy Values
+// ####################### Truthy and Falsy Values #######################
 
 // 5 falsy values : 0, '', undefined, null, NaN
 
-console.log(Boolean()); // will return a boolean value; either true or false
+// console.log(Boolean()); // will return a boolean value; either true or false
 
 const money = 0; // 0 is a falsey value so the else block will be executed
 
-if (money) {
-    console.log("Don't spend it all ;)");
-} else {
-    console.log('You should get a job!');
-}
+// if (money) {
+//     console.log("Don't spend it all ;)");
+// } else {
+//     console.log('You should get a job!');
+// }
 
-let height; // there is no value assigned.
-if(height) {  
-    console.log('YAY! Height is defined');
-} else {
-    console.log('Height is undefined'.toUpperCase()); // returns this block of code
-}
+// let height; // there is no value assigned.
+// if(height) {  
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is undefined'.toUpperCase()); // returns this block of code
+// }
 
-// Equality Operators: == vs ===
+// ####################### Equality Operators: == vs === #######################
+
+// const age2 = 18;
+// if (age2 === 18) console.log('You just became an adult :D '); // strict equality operator // always default to the triple equal operator
+
+// const age3 = '18'
+// if (age3 == 18) console.log('You just became an adult :D '); // loose equality operator
+
+// const favourite = Number (prompt("What's your favorite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if(favourite === 23) { // 22 == 23
+//     console.log(`Cool! 23 is an amazing number!`);
+// } else if (favourite === 7) {
+//     console.log(`7 is also a cool number!`);
+// } else {
+//     console.log('Number is not 23 or 7 :(')
+// }
+
+
+// if (favourite !== 23) console.log('Why not 23?');
+
+// ####################### Practice Assignment: #######################
+/*
+1. Declare a variable 'numNeighbours' based on a prompt input like this:
+prompt('How many neighbour countries does your country
+have?');
+*/
+
+// const numNeighbours = Number(prompt ('How many neighbour countries does your country have?'));
+// console.log(numNeighbours);
+
+/*
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+== for now)
+*/
+
+// if (numNeighbours == 1) console.log(`Only 1 border!`);
+
+/*
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+is greater than 1
+*/
+
+// if (numNeighbours === 1) {
+//     console.log(`Only 1 border!`);
+// } else if  (numNeighbours > 1) {
+//     console.log(`More than 1 border`)
+// }
+
+/*
+4. Use an else block to log 'No borders' (this block will be executed when
+'numNeighbours' is 0 or any other value)
+*/
+
+// else {
+//     console.log('No Borders!');
+// }
+
+/*
+5. Test the code with different values of 'numNeighbours', including 1 and 0.
+*/
+
+
+
+/*
+6. Change == to ===, and test the code again, with the same values of
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+is this happening?
+*/
+
+/*
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+when you input 1
+*/
+
+/*
+8. Reflect on why we should use the === operator and type conversion in this
+situation
+*/
+
+// it is best to use the === operator because the answer requires a return of an integer / number. We don't 
+// necessarily need a string as an answer. Also helps us prevent bugs.
 
